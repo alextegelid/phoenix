1d5fdb4ffc61a6a0800afcb72fd7d89cdfa303ee
Alexander Tegelid <alex@humblegiant.se>
2022-06-04 14:05:34 +0200

    Add window gap setting to center and thirds rotation
    Known issue: thirds is not exactly right calculated

--- shortcuts/center_rotation.js
+++ shortcuts/center_rotation.js
@@ -26,10 +26,10 @@ setKeyHandler ( 'c', HYPER, () => {
 	const rotationValues = centerRotationQueue[currentCenterRotationKeys[hash]];
 
 	const nextFrame = {
-		x: sFrame.width * rotationValues[0],
-		y: sFrame.y + sFrame.height * rotationValues[1],
-		width: sFrame.width * (rotationValues[2]-rotationValues[0]),
-		height: sFrame.height * (rotationValues[3]-rotationValues[1])
+		x: sFrame.width * rotationValues[0] + WINDOW_GAP,
+		y: sFrame.y + sFrame.height * rotationValues[1] + WINDOW_GAP,
+		width: sFrame.width * (rotationValues[2]-rotationValues[0]) - WINDOW_GAP * 2,
+		height: sFrame.height * (rotationValues[3]-rotationValues[1]) - WINDOW_GAP * 2
 	};
 
 	window.setFrame(nextFrame);
