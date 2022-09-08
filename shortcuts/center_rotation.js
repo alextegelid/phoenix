8360041e1ee79379db5d8e86c776a73b85ebdba9
Alexander Tegelid <alex@tegelid.se>
2022-09-08 13:16:51 +0200

    Add ultra wide support

--- shortcuts/center_rotation.js
+++ shortcuts/center_rotation.js
@@ -2,6 +2,12 @@
 /* EXPAND */
 
 const centerRotationQueue = [
+	[0, 0, 1, 1],
+	[0.15, 0.1, 0.7, 0.85],
+	[0.15, 0, 0.7, 1]
+];
+
+const centerRotationQueueUltraWide = [
 	[0.1, 0.1, 0.5, 0.85],
 	[0.1, 0, 0.5, 1],
 	[0, 0, 0.6666, 1],
@@ -24,8 +30,17 @@ setKeyHandler ( 'c', HYPER, () => {
 
 	const sFrame = screen.flippedVisibleFrame();
 
+	// Check if the window is ultra wide and select rotationQueue accordingly
+	const isUltraWide = sFrame.width / sFrame.height > 2;
+	const rotationQueue = isUltraWide ? centerRotationQueueUltraWide : centerRotationQueue;
+
+	// If the next rotation doesn't exist due to having changed screens (resolution) then reset to 0
+	if (!rotationQueue[currentCenterRotationKeys[hash]]) {
+		currentCenterRotationKeys[hash] = 0;
+	}
+
 	// Extract the values from the array.
-	const [origX, origY, origWidth, origHeight] = centerRotationQueue[currentCenterRotationKeys[hash]];
+	const [origX, origY, origWidth, origHeight] = rotationQueue[currentCenterRotationKeys[hash]];
 
 	// Calculate new frame.
 	// Use percentage of screen size if the value is between 0 and 1.
@@ -45,7 +60,7 @@ setKeyHandler ( 'c', HYPER, () => {
 	window.setFrame(nextFrame);
 
 	currentCenterRotationKeys[hash]++;
-	if (currentCenterRotationKeys[hash] > centerRotationQueue.length-1) {
+	if (currentCenterRotationKeys[hash] > rotationQueue.length-1) {
 		currentCenterRotationKeys[hash] = 0;
 	}
 
